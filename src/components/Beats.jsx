8eb335a5bdc0b8671951557c7edcc988d594f0dc
breab <brea.borlas@gmail.com>
2018-06-29 12:09:12 -0700

    Add in array of objects to loop through

--- src/components/Beats.jsx
+++ src/components/Beats.jsx
@@ -2,6 +2,153 @@ import React from 'react';
 import SingleBeat from './SingleBeat';
 
 function Beats(){
+  const availableProduce = [
+  {
+    beatNumber: 1,
+    sounds: [
+      "kick.mp3",
+      "snare.mp3",
+      "hho.mp3",
+      "hhc.mp3"
+    ]
+  },
+  {
+    beatNumber: 2,
+    sounds: [
+      "kick.mp3",
+      "snare.mp3",
+      "hho.mp3",
+      "hhc.mp3"
+    ]
+  },
+  {
+    beatNumber: 3,
+    sounds: [
+      "kick.mp3",
+      "snare.mp3",
+      "hho.mp3",
+      "hhc.mp3"
+    ]
+  },
+  {
+    beatNumber: 4,
+    sounds: [
+      "kick.mp3",
+      "snare.mp3",
+      "hho.mp3",
+      "hhc.mp3"
+    ]
+  },
+  {
+    beatNumber: 5,
+    sounds: [
+      "kick.mp3",
+      "snare.mp3",
+      "hho.mp3",
+      "hhc.mp3"
+    ]
+  },
+  {
+    beatNumber: 6,
+    sounds: [
+      "kick.mp3",
+      "snare.mp3",
+      "hho.mp3",
+      "hhc.mp3"
+    ]
+  },
+  {
+    beatNumber: 7,
+    sounds: [
+      "kick.mp3",
+      "snare.mp3",
+      "hho.mp3",
+      "hhc.mp3"
+    ]
+  },
+  {
+    beatNumber: 8,
+    sounds: [
+      "kick.mp3",
+      "snare.mp3",
+      "hho.mp3",
+      "hhc.mp3"
+    ]
+  },
+  {
+    beatNumber: 9,
+    sounds: [
+      "kick.mp3",
+      "snare.mp3",
+      "hho.mp3",
+      "hhc.mp3"
+    ]
+  },
+  {
+    beatNumber: 10,
+    sounds: [
+      "kick.mp3",
+      "snare.mp3",
+      "hho.mp3",
+      "hhc.mp3"
+    ]
+  },
+  {
+    beatNumber: 11,
+    sounds: [
+      "kick.mp3",
+      "snare.mp3",
+      "hho.mp3",
+      "hhc.mp3"
+    ]
+  },
+  {
+    beatNumber: 12,
+    sounds: [
+      "kick.mp3",
+      "snare.mp3",
+      "hho.mp3",
+      "hhc.mp3"
+    ]
+  },
+  {
+    beatNumber: 13,
+    sounds: [
+      "kick.mp3",
+      "snare.mp3",
+      "hho.mp3",
+      "hhc.mp3"
+    ]
+  },
+  {
+    beatNumber: 14,
+    sounds: [
+      "kick.mp3",
+      "snare.mp3",
+      "hho.mp3",
+      "hhc.mp3"
+    ]
+  },
+  {
+    beatNumber: 15,
+    sounds: [
+      "kick.mp3",
+      "snare.mp3",
+      "hho.mp3",
+      "hhc.mp3"
+    ]
+  },
+  {
+    beatNumber: 16,
+    sounds: [
+      "kick.mp3",
+      "snare.mp3",
+      "hho.mp3",
+      "hhc.mp3"
+    ]
+  }
+];
+
   return (
     <div>
       <SingleBeat />
