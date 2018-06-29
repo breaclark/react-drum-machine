5c1ce1c47d9ffd30707f97ee87ec28af94982b25
breab <brea.borlas@gmail.com>
2018-06-29 15:31:24 -0700

    Add media query for save settings

--- src/components/SaveSettings.jsx
+++ src/components/SaveSettings.jsx
@@ -26,6 +26,21 @@ function SaveSettings(){
           margin: 4px;
           width: 80px;
         }
+
+        @media screen and (max-width: 580px) {
+          .save-settings input {
+            font-size: 10px;
+            height: 16px;
+          }
+
+          .save-settings button{
+            font-size: 12px;
+            padding: 0px;
+            margin: 2px;
+            width: 40px;
+          }
+
+        }
       `}</style>
       <input type="text" placeholder="Enter name to save"/>
       <button>Save</button>
