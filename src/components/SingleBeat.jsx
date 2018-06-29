37a5ab5156a7c1b4b0550d53b18aef0d795e5531
breab <brea.borlas@gmail.com>
2018-06-29 10:18:21 -0700

    Refactor so that the type of sound will be passed through props, no need for different types of sounds

--- src/components/SingleBeat.jsx
+++ src/components/SingleBeat.jsx
@@ -1,18 +1,12 @@
 import React from "react";
 import BeatNumber from "./BeatNumber";
-import Kick from "./Kick";
-import Snare from "./Snare";
-import HHO from "./HHO";
-import HHC from "./HHC";
+import Sound from "./Sound";
 
 function SingleBeat(){
   return (
     <div>
       <BeatNumber />
-      <Kick />
-      <Snare />
-      <HHO />
-      <HHC />
+      <Sound />
     </div>
   );
 }
--- src/components/Sound.jsx
+++ src/components/Sound.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-function HHO(){
+function Sound(){
   return (
     <div>
-      <h5>HHO goes here</h5>
+      <h5>Sound goes here</h5>
     </div>
   );
 }
 // Put in props to pass down the sound
-export default HHO;
+export default Sound;
