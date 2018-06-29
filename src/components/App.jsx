2c2c55c0b2ebf9ffd76e2ca713ea94a80cae2228
breab <brea.borlas@gmail.com>
2018-06-29 09:16:56 -0700

    Add styling to app and header, import google fonts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,15 @@ import Header from "./Header";
 function App(){
   return (
     <div>
+      <link href="https://fonts.googleapis.com/css?family=Ubuntu+Mono" rel="stylesheet" />
+      <style jsx global>{`
+        body {
+          background-color: #383838;
+        }
+        * {
+          font-family: 'Ubuntu Mono', monospace;
+        }
+      `}</style>
       <Header />
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,16 @@ import React from "react";
 
 function Header(){
   return (
-    <div>
-      <h1>Henlo!</h1>
+    <div className="header">
+      <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" />
+      <style jsx>{`
+        .header h1 {
+          color: white;
+          font-family: 'Lobster', cursive;
+        }
+      `}</style>
+      <h1>React Drum Machine Clone</h1>
+      <p>Test text</p>
     </div>
   );
 }
